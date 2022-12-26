9fbd29739ff2a32d44207b864074fe479a4ee92a
ray396 <rayssadantas31@gmail.com>
2022-12-26 11:43:21 -0300

    criação de projeto

--- github-proj/src/components/User.tsx
+++ github-proj/src/components/User.tsx
@@ -2,6 +2,8 @@ import { UserProps } from "../types/user";
 
 import {MdLocationPin} from "react-icons/md";
 
+import { Link } from "react-router-dom";
+
 const User = ({login, avatar_url, followers, following, location}: UserProps) => {
     return(
         <div>
@@ -16,10 +18,14 @@ const User = ({login, avatar_url, followers, following, location}: UserProps) =>
                     <p>Seguidores:</p>
                     <p>{followers}</p>
                 </div>
-                
+                <div>
+                    <p>Seguindo:</p>
+                    <p>{following}</p>
+                </div>
             </div>
+            <Link to={`/repos/${login}`}>Ver melhores projeto</Link>
         </div>
-    )
-}
+    );
+};
 
-export default User
+export default User;
